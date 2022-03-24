1306f4a22212487b93ef7ff917f7dce1f6c2c28d
upendra <upendrarajput9911@gmail.com>
2022-03-24 22:37:55 +0530

    block-BNaacj

---
+++ block-BNaacj/simple/app.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+var userSchema = new Schema({
+    name:String,
+    age:Number,
+    password:{min:5,max:15},
+    createdAt:{type:Date,default:today}
+})
